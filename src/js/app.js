791741ae43198a85eb77378747d21ded2f7ef66c
Pauli <susundberg@gmail.com>
2016-04-27 17:19:24 +0300

    Now it should send the options

---
+++ src/js/app.js
@@ -0,0 +1,32 @@
+Pebble.addEventListener('ready', function() {
+  console.log('PebbleKit JS ready!');
+});
+
+Pebble.addEventListener('showConfiguration', function() {
+  var url = 'https://rawgit.com/susundberg/pebble-countit-all/master/config/index.html';
+  console.log('Showing configuration page: ' + url);
+  Pebble.openURL(url);
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  var config = JSON.parse(decodeURIComponent(e.response));
+  console.log('Configuration page returned: ' + JSON.stringify( config ));
+
+  var dict = {};
+  for ( var loop_button = 1; loop_button < 3; loop_button ++ )
+  {
+     var prefix="button" + loop_button;
+     
+     var key_icon = (prefix+"_icon").toUpperCase();
+     var key_type = (prefix+"_type").toUpperCase();
+     dict[key_icon] = config[ prefix + "-icon" ].toUpperCase();
+     dict[key_type] = config[ prefix + "-type" ].toUpperCase();
+  }
+  
+  // Send to watchapp
+  Pebble.sendAppMessage( dict, function() {
+    console.log('Send successful: ' + JSON.stringify(dict));
+  }, function() {
+    console.log('Send failed: ' + JSON.stringify(dict) );
+  });
+});
