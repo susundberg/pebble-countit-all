8ffad6eab60b73b4daec372faa90936463b46462
Pauli <susundberg@gmail.com>
2016-05-21 15:40:52 +0300

    Prepare the release 1 without the download options

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 
 
-var TARGET_HOST = 'https://rawgit.com/susundberg/pebble-countit-all/master/config/';
-// var TARGET_HOST = 'https://cdn.rawgit.com/susundberg/pebble-countit-all/master/config/';
+// var TARGET_HOST = 'https://rawgit.com/susundberg/pebble-countit-all/master/config/';
+var TARGET_HOST = 'https://cdn.rawgit.com/susundberg/pebble-countit-all/master/config/';
 
 // var TARGET_HOST = 'http://localhost:8000/';
 
